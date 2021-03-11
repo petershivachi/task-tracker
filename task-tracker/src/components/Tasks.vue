f12ab6aad7b3106820139a92b37f18122f73d0ab
petershivachi <shitsukanepeter@gmailc.om>
2021-03-11 15:16:13 +0300

    emit events, toggle reminder

---
+++ task-tracker/src/components/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <div :key="task.id" v-for="task in tasks">
+   <Task 
+   @toggle-task="$emit('toggle-task', task.id)"
+   @delete-task="$emit('delete-task', task.id)" :task="task"/>
+ </div>
+</div>
+</template>
+
+<script>
+import Task from './Task.vue';
+
+export default {
+  name: 'Tasks',
+  props: {
+    tasks: Array
+  },
+
+  components: {
+    Task
+  }
+}
+</script>
