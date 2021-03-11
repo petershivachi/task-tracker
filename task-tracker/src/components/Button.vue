08f176bbf0e2a517d06637a4a66567a11a6e26e3
petershivachi <shitsukanepeter@gmailc.om>
2021-03-11 23:51:59 +0300

    json server setup, refractered to use backend

--- task-tracker/src/components/Button.vue
+++ task-tracker/src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :style="{background: color}" class="btn">{{ text }}</button>
+  <button @click="onClick()" :style="{background: color}" class="btn">{{ text }}</button>
 </template>
 
 <script>
@@ -8,6 +8,12 @@ export default {
   props: {
     text: String,
     color: String
+  },
+
+  methods: {
+    onClick(){
+      this.$emit('btn-click');
+    }
   }
 }
 </script>
